8b1eae80f837db0de481d816f6679a3914033e75
breslowday <deepit.bhatia@psu.edu>
2025-04-30 17:44:32 -0400

    clarified language on run_this.R

--- run_this.R
+++ run_this.R
@@ -3,8 +3,10 @@ library(here)
 # create functions
 source("01_functions.R")
 
-# create estimates for all 4 regions
+# create 4 estimates for 4 regions 
 source("10_create_CI_tables_indiv.R")
+
+# create 3 estimates for 4 regions where regions 3+4 are considered 1 "region"
 source("11_create_CI_tables_modified.R")
 
 
